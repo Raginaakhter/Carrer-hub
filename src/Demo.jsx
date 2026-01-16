693b5e5a2b0ef1cfd37066e9d71aa97e2862ea37
Raginaakhter <raginaakhter052@gmail.com>
2026-01-16 23:55:19 +0600

    updated code

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            Lorem ipsum  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt voluptate assumenda exercitationem suscipit quasi! Et, placeat temporibus? Quaerat, quia quam culpa nesciunt qui ipsa, earum quod hic soluta, dolore alias.
+            earum quod hic soluta, dolore alias.
         </div>
     );
 };
