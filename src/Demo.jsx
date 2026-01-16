eb8ded6aea0fdb6b3f951659c83c9d576ea667ee
Raginaakhter <raginaakhter052@gmail.com>
2026-01-17 00:06:59 +0600

    add lorem

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,7 +3,8 @@
 const Demo = () => {
     return (
         <div>
-            <p className="bg-blue-800">Lorem ing elit. liquid doloremque eligendi illo
+            <p className="bg-blue-800">Lorem ing elit. liquid doloremque eligendi illo Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, placeat sed atque est quos a? Eaque magnam beatae obcaecati error sit commodi odit, quidem sint officiis. Iusto, laudantium. Beatae, 
+                optio?
                  accusantium iste perferre. Voluptatum!</p>
         <button className="bg-black text-white">Click this</button>
         </div>
