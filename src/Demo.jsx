7e1d349655b630172dfdb63cdac8e5c1fbb45112
Raginaakhter <raginaakhter052@gmail.com>
2026-01-17 00:07:43 +0600

    updated code

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            <p className="bg-blue-800">Lorem ing elit. liquid doloremque eligendi illo Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, placeat sed atque est quos a? Eaque magnam beatae obcaecati error sit commodi odit, quidem sint officiis. Iusto, laudantium. Beatae, 
+            <p className="bg-blue-800">Leligendi illo Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, placeat sed atque est quos a? Eaque magnam beatae obcaecati error sit commodi odit, quidem sint officiis. Iusto, laudantium. Beatae, 
                 optio?
                  accusantium iste perferre. Voluptatum!</p>
         <button className="bg-black text-white">Click this</button>
