dc5dcf4b97ca589bd15e5d9bd3554c0eb9c5edd0
Raginaakhter <raginaakhter052@gmail.com>
2026-01-17 00:04:47 +0600

    update button

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -5,7 +5,7 @@ const Demo = () => {
         <div>
             <p className="bg-blue-800">Lorem ing elit. est eos, vero nihil amet aliquid doloremque eligendi illo
                  accusantium iste perferendis maxime labore. Voluptatum!</p>
-        <button className="bg-black text-white">Click here</button>
+        <button className="bg-black text-white">Click this</button>
         </div>
     );
 };
