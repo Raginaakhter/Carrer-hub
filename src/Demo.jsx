00079dc9e9e5679d398e609cc443e47547af979f
Raginaakhter <raginaakhter052@gmail.com>
2026-01-17 00:03:34 +0600

    add updated p tag

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,7 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            <p className="bg-blue-800">Lorem ing elit. Aut nemo sint ut quidem sunt, est eos, vero nihil amet aliquid doloremque eligendi illo
+            <p className="bg-blue-800">Lorem ing elit. est eos, vero nihil amet aliquid doloremque eligendi illo
                  accusantium iste perferendis maxime labore. Voluptatum!</p>
         <button className="bg-black text-white">Click here</button>
         </div>
